dce859de22c02808773eeb2dc3911904c41e72ed
aliseylaneh <aliseylaneh@gmail.com>
2022-12-04 00:12:32 +0330

    3: Expanded widgets

--- lib/main.dart
+++ lib/main.dart
@@ -14,18 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.red,
-      ),
       home: Row(
         children: [
           BlueBox(),
@@ -34,9 +22,8 @@ class MyApp extends StatelessWidget {
             flex: 1,
             child: BlueBox(),
           ),
-          Flexible(
-            fit: FlexFit.tight,
-            flex: 1,
+          Expanded(
+            flex: 2,
             child: BlueBox(),
           ),
         ],
@@ -49,7 +36,7 @@ class BlueBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 70,
+      width: 50,
       height: 50,
       decoration: BoxDecoration(
         borderRadius: const BorderRadius.all(Radius.circular(20.0)),
@@ -57,6 +44,7 @@ class BlueBox extends StatelessWidget {
         border: Border.all(),
       ),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: const [
           Icon(size: 30, Icons.add_call),
         ],
